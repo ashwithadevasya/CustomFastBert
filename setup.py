303260ecd792d0e2d42db4bfbd0e8f7446b4c39a
ashwikishan1996 <56867178+ashwikishan1996@users.noreply.github.com>
2020-07-19 19:46:52 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CustomFastBert',
+    version='1.0-SNAPSHOT',
+    packages=['ashwitha'],
+    url='https://github.com/ashwithadevasya/CustomFastBert',
+    license='',
+    author='Ashwitha Devasya',
+    author_email='ashwithasathi@gmail.com',
+    description=''
+)
